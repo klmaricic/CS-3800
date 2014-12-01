8dbfdd6f09e061d988398ac28a4fe064d3f3f1dc
Kelsey Maricic <klmhw9@mst.edu>
2014-12-01 14:32:22 -0600

    Created hw3
    Created hw3

---
+++ assgn3/hw3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string hostname, username;
+
+  cout << "Welcome! Please enter a hostname for the server" << endl;
+  cin >> hostname;
+
+  cout <<"What username would you like to use?" << endl;
+  cin >> username;
+}
