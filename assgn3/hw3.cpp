903c3ce7371b1be6bd68ac015514461df8a3ee3e
Kelsey Maricic <klmhw9@mst.edu>
2014-12-01 15:00:59 -0600

    Added some code for client side
    Added some code for client side

--- assgn3/hw3.cpp
+++ assgn3/hw3.cpp
@@ -1,13 +1,23 @@
+//Authors: Kelsey Maricic, John Tegtmeyer
+
 #include <iostream>
+#include <signal.h>
+#include <sys/socket.h>
 using namespace std;
+
+struct client {
+  string userName;
+  int clientId = 0;
+} user;
 
 int main()
 {
-  string hostname, username;
+  string hostname;
+  
 
   cout << "Welcome! Please enter a hostname for the server" << endl;
   cin >> hostname;
 
   cout <<"What username would you like to use?" << endl;
-  cin >> username;
+  cin >> user.userName;
 }
